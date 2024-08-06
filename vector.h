d7662139504d754772c4e1108fc02f074e7dd974
GiovanyH <56926698+GiovanyH@users.noreply.github.com>
2024-08-05 22:22:04 -0300

    Update vector.h

--- vector.h
+++ vector.h
@@ -2,34 +2,18 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-typedef struct {
-    int len;
-    int cap;
-} array_header;
-
 #define new_vector(TYPE, NAME) \
-    TYPE *NAME = (void*)(((array_header*)malloc(sizeof(array_header) + sizeof(TYPE)*8)) + 1); \
-    array_header *HEADER##NAME = vec_get_hdr(NAME); \
-    HEADER##NAME->cap = 8; \
-    HEADER##NAME->len = 0;
+    TYPE *NAME = (TYPE*)((int*)malloc(2 * sizeof(int) + sizeof(TYPE) * 8) + 2); \
+    ((int *)NAME)[-2] = 0; ((int *)NAME)[-1] = 8;
 
-#define vec_get_hdr(SB)      ((array_header *)(SB) - 1)
+#define vec_get_hdr(SB) ((int*)(SB) - 2)
+#define vec_push(SB, ELEM) vec_expand((void**)&(SB), sizeof(ELEM), vec_get_hdr(SB)), (SB)[vec_get_hdr(SB)[0]++] = (ELEM)
+#define vec_pop(SB, TYPE) vec_shrink((void**)&(SB), sizeof(TYPE), vec_get_hdr(SB))
 
-#define vec_push(SB, ELEM)   (vec_expand((void**)&(SB), sizeof (*(SB))), (SB)[vec_get_hdr(SB)->len++] = (ELEM))
-#define vec_pop(SB)          (vec_shrink((void**)&(SB), sizeof(*(SB))))
-
-static inline void vec_expand(void **sb, size_t isz) {
-    array_header *h = vec_get_hdr(*sb);
-    if (h->len >= h->cap) {
-        h->cap *= 2;
-        *sb = (void*)(((array_header*)realloc(h, sizeof(array_header) + h->cap*isz)) + 1);
-    }
+static inline void vec_expand(void **sb, size_t isz, int *h) {
+    if (h[0] >= h[1]) *sb = (void*)((int*)realloc(h, 2 * sizeof(int) + (h[1] *= 2) * isz) + 2);
 }
 
-static inline void vec_shrink(void **sb, size_t isz) {
-    array_header *h = vec_get_hdr(*sb);
-    if (--h->len < h->cap - 8) {
-        h->cap -= 8;
-        *sb = (void*)(((array_header*)realloc(h, sizeof(array_header) + h->cap*isz)) + 1);
-    }
+static inline void vec_shrink(void **sb, size_t isz, int *h) {
+    if (--h[0] < h[1] - 8) *sb = (void*)((int*)realloc(h, 2 * sizeof(int) + (h[1] -= 8) * isz) + 2);
 }
