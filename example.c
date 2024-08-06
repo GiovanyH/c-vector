6b300014e69aae325ed8c0fe3190512be2ba9669
GiovanyH <56926698+GiovanyH@users.noreply.github.com>
2024-08-05 22:15:34 -0300

    Update example.c

--- example.c
+++ example.c
@@ -1,23 +1,15 @@
 #include "vector.h"
-#include <time.h>
 
-int main(void){
-	srand(time(NULL));
-
-	new_vector(int, v); // declaring the vector
-	printf("vector capacity is %d integers\n", vec_get_capacity(v));
-
-	#define NITERS 10
-	for (int i = 0; i < NITERS; i++) {
-		vec_push(v, rand());
-	}
-	printf("pushed 10 elements to vector\n");
-	
-	printf("length: %d, capacity: %d\n", vec_get_len(v), vec_get_capacity(v));
-
-	for (int i = vec_get_len(v); i > 7; i--) vec_pop(v);
-	printf("popped 2 elements from vector\n");
-	printf("length: %d, capacity: %d\n", vec_get_len(v), vec_get_capacity(v));
-
-	vec_free(v);
+int main() {
+    new_vector(int, vec);
+    for (int i = 0; i < 20; ++i) {
+        vec_push(vec, i);
+        printf("Pushed: %d, Length: %d, Capacity: %d\n", i, vec_get_hdr(vec)[0], vec_get_hdr(vec)[1]);
+    }
+    for (int i = 0; i < 10; ++i) {
+        vec_pop(vec, int);
+        printf("Popped, Length: %d, Capacity: %d\n", vec_get_hdr(vec)[0], vec_get_hdr(vec)[1]);
+    }
+    free(vec_get_hdr(vec));
+    return 0;
 }
